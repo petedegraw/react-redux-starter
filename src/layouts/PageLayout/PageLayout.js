e3549b792a80e9e480101d212ef7070d23deed0f
Pete DeGraw <petedegraw@gmail.com>
2017-06-15 21:01:17 -0700

    Adds list of events to about page

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types'
 import './PageLayout.scss'
 
 export const PageLayout = ({ children }) => (
-  <div className='container text-center'>
+  <div>
+    <div className='container text-center'>
     <h1>React Redux Starter Kit</h1>
     <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
     {' · '}
     <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
     {' · '}
     <Link to='/about' activeClassName='page-layout__nav-item--active'>About</Link>
-    <div className='page-layout__viewport'>
+    </div>
+    <div className='container page-layout__viewport'>
       {children}
     </div>
   </div>
--- src/routes/About/components/AboutView.js
+++ src/routes/About/components/AboutView.js
@@ -2,9 +2,21 @@ import React from 'react'
 // import DuckImage from '../assets/Duck.jpg'
 // import './HomeView.scss'
 
+const events = [
+  'Event one',
+  'Event two',
+  'Event three'
+];
+const listEvents = events.map((events) =>
+  <h4>{events}</h4>
+);
+
 export const AboutView = () => (
   <div>
     <h4>About Page</h4>
+    <div>
+      {listEvents}
+    </div>
   </div>
 )
 
