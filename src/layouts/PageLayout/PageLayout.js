45ed06bae2b7e7997680b12375d71049a2f6fd59
Pete DeGraw <petedegraw@gmail.com>
2017-06-15 20:43:12 -0700

    Adds about route, view.

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -9,6 +9,8 @@ export const PageLayout = ({ children }) => (
     <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
     {' · '}
     <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
+    {' · '}
+    <Link to='/about' activeClassName='page-layout__nav-item--active'>About</Link>
     <div className='page-layout__viewport'>
       {children}
     </div>
---
+++ src/routes/About/components/AboutView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import DuckImage from '../assets/Duck.jpg'
+// import './HomeView.scss'
+
+export const AboutView = () => (
+  <div>
+    <h4>About Page</h4>
+  </div>
+)
+
+export default AboutView
